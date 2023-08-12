ac62f8c6d88fb5c7e18be497d3583cbedbe3b24a
josh-001 <joshuakakarla555@gmail.com>
2023-08-12 20:31:04 +0530

    zeros to last

---
+++ LEET CODE/ARRAYS/283. Move Zeroes.py
@@ -0,0 +1,29 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)==1:
+            return nums
+        c=0
+        for i in range(len(nums)):
+            if 0 in nums:
+                c=c+1
+                nums.remove(0)
+        for i in range(c):
+            nums.append(0)

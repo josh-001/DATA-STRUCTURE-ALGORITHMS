994f3fbc15556c9dce6d2c1aa0f1c03f6d48b19c
josh-001 <joshuakakarla555@gmail.com>
2023-08-16 23:58:49 +0530

    daily challenge

---
+++ LEET CODE/30 Days of August-2023/Week3 14-08-20 to 20-08-2023/239. Sliding Window Maximum.py
@@ -0,0 +1,44 @@
+# ou are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+# Return the max sliding window.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        if k == 1:
+            return nums
+        deq = deque()
+        result = []
+
+        for i in range(len(nums)):
+            while deq and deq[0] < i - k + 1:
+                deq.popleft()
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+            deq.append(i)
+            if i >= k - 1:
+                result.append(nums[deq[0]])      
+  
+        return result

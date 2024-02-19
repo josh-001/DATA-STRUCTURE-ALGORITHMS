d34d56c8941f19aaa9866edf2a628bdb5f0a45aa
bhanu <joshuakakarla555@gmail.com>
2024-02-19 20:34:30 +0530

    power_of_two

---
+++ LEET CODE/30 Days of Feburary 2024/231. Power of Two.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<=0:
+            return False
+        while n>0:
+            if n==1:
+                return True
+            elif n%2==0:
+                n=n/2
+                continue
+            else:
+                return False
+        return True
+        
